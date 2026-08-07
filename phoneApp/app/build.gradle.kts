ea31e32222d88921733955f000021d69b67b4aef
曾理 <10399488+zengli199447@user.noreply.gitee.com>
2026-08-07 22:24:21 +0800

    Bump Android version to 0.1.6

--- phoneApp/app/build.gradle.kts
+++ phoneApp/app/build.gradle.kts
@@ -24,8 +24,8 @@ android {
         applicationId = "com.alicia.cloudstorage.phone"
         minSdk = 26
         targetSdk = 34
-        versionCode = 6
-        versionName = "0.1.5"
+        versionCode = 7
+        versionName = "0.1.6"
 
         buildConfigField("String", "DEFAULT_API_BASE_URL", "\"${resolveApiBaseUrl(project)}\"")
     }
